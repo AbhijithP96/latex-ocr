26b7398fa3a73a166f377d2e0aef32827cfa3d83
AbhijithP96 <premraj.abhijith@gmail.com>
2026-01-12 17:06:23 +0400

    code formatted with black

--- src/backend.py
+++ src/backend.py
@@ -4,8 +4,12 @@ import base64
 from langchain_nvidia_ai_endpoints import ChatNVIDIA
 from langchain_core.messages import HumanMessage
 
+# Load environment variables
 load_dotenv()
-llm = ChatNVIDIA(model=os.environ['NVIDIA_MODEL'],nvidia_api_key=os.environ['NVIDIA_API_KEY'])
+llm = ChatNVIDIA(
+    model=os.environ["NVIDIA_MODEL"], nvidia_api_key=os.environ["NVIDIA_API_KEY"]
+)
+# Define the instruction for the LLM
 instruction = """Understand the mathematical equation in the provided image and output the corresponding LaTeX code.
                             Here are some guidelines you MUST follow or you will be penalized:
                             - NEVER include any additional text or explanations.
@@ -14,13 +18,21 @@ instruction = """Understand the mathematical equation in the provided image and 
                             - NEVER add documentclass, packages or begindocument.
                             - DO NOT explain the symbols used in the equation.
                             - Output only the LaTeX code corresponding to the mathematical equations in the image."""
-                            
+
+
 def run_inference(img_data):
+    """
+    Method to run inference on the provided image data using NVIDIA AI Models.
 
-    b64 = base64.b64encode(img_data).decode("utf-8")
-    data_url = f"data:image/png;base64,{b64}"
+    Args:
+        img_data: _image data in bytes format
 
+    Returns:
+        LLM response containing the LaTeX code.
+    """
 
+    b64 = base64.b64encode(img_data).decode("utf-8")  # Convert bytes to base64 string
+    data_url = f"data:image/png;base64,{b64}"  # Create data URL
 
     resp = llm.invoke(
         [
@@ -29,13 +41,11 @@ def run_inference(img_data):
                     {"type": "text", "text": instruction},
                     {
                         "type": "image_url",
-                        "image_url": {
-                            "url": data_url
-                        },
+                        "image_url": {"url": data_url},
                     },
                 ]
             )
         ]
-    )
+    )  # Invoke the LLM with the image and instruction
 
     return resp.content
